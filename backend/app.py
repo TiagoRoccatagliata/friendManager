1e90fbe10b87b68eb43755bdc2e79f8e87773b5b
Tiago Roccatagliata <tiagoroccatagliata@Mac.fibertel.com.ar>
2024-12-11 13:06:27 -0300

    Fix Route

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,30 @@
+import os
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
-CORS(app)  # Permite todas las solicitudes de todos los orígenes
+CORS(app)
 
+# Configuración de la base de datos
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
-dist_folder = os.path.join(frontend_folder, "dist")
+# Path al directorio "dist" del frontend
+dist_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "frontend", "dist")
 
-# Servir archivos estáticos del frontend
+# Servir archivos estáticos
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists("static/" + path):
-        return send_from_directory("static", path)
+    if path != "" and os.path.exists(os.path.join(dist_folder, path)):
+        return send_from_directory(dist_folder, path)
     else:
-        return send_from_directory("static", "index.html")
+        return send_from_directory(dist_folder, "index.html")
 
-# Importar rutas después de inicializar app y db
+# Importar rutas después de inicializar `app` y `db`
 from routes import *
 
 # Crear la base de datos
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL =  "http://127.0.0.1:5000/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://127.0.0.1:5000/api" // Backend en desarrollo
+    : "/api"; // Backend en producción
 
 const api = axios.create({
   baseURL: BASE_URL,
